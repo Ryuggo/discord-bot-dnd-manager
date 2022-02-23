10703e802ca802a01ae166701752fdbe15fbdfb2
Hugo Fr <51454951+Ryuggo@users.noreply.github.com>
2022-02-23 10:01:06 +0000

    Fixed remove not displaying the name

--- buttons/characterRemove.js
+++ buttons/characterRemove.js
@@ -14,9 +14,11 @@ module.exports = {
 			let name = '';
 			let i = 0;
 			bd["sheets"].forEach(sheets => {
-				if(i == nb) name = sheets[0].Character[0].Name;
+				if(i == nb) name = sheets[0].Character[1].Name;
 				i++;
 			});
+
+			if(bd["charSelected"] == nb) bd["charSelected"] = 0;
 			
 			bd["sheets"].splice(nb, 1);
 			
